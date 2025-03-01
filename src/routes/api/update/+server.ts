ed84057ef26c30aa8153aff49f4d946efafdbeb7
ArkStructCodes <153435292+ArkStructCodes@users.noreply.github.com>
2025-03-01 15:34:44 +0530

    Configure Vercel build

--- src/routes/api/update/+server.ts
+++ src/routes/api/update/+server.ts
@@ -4,9 +4,10 @@ import type { RequestHandler } from "./$types";
 import { state } from "$lib/server";
 
 export const GET: RequestHandler = ({ request }) => {
+  console.log("Got update request with the following headers:", request.headers);
   if (request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
-    return error(401, "this endpoint is not for external use");
+    return error(401, "This endpoint is not for external use.");
   }
   state.updateAnswer();
-  return json({ message: "answer updated", data: state.answer }, { status: 202 });
+  return json({ message: "Answer updated.", data: state.answer }, { status: 202 });
 };
